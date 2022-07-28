392eb84000ca1d8dc57de78f79a5853956c72e93
Your Name <you@example.com>
2022-07-28 23:09:41 +0530

    change

---
+++ build/precache-manifest.d288bedc6a6399940f13dc4e0441b508.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0641ad2c98242de9b1e5d63fcb318361",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f6098819200a8f1bfebf",
+    "url": "/Portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "f122023867f5aed8fbe9",
+    "url": "/Portfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "826415b5e96e3a07eef2",
+    "url": "/Portfolio/static/css/main.44774f47.chunk.css"
+  },
+  {
+    "revision": "e5c36e4e672342b193c1",
+    "url": "/Portfolio/static/js/2.975fb9c6.chunk.js"
+  },
+  {
+    "revision": "2a11267bc005dc2fa0b6098090277887",
+    "url": "/Portfolio/static/js/2.975fb9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6098819200a8f1bfebf",
+    "url": "/Portfolio/static/js/3.befe9dc4.chunk.js"
+  },
+  {
+    "revision": "f122023867f5aed8fbe9",
+    "url": "/Portfolio/static/js/4.311a6ec1.chunk.js"
+  },
+  {
+    "revision": "826415b5e96e3a07eef2",
+    "url": "/Portfolio/static/js/main.8c9bacdc.chunk.js"
+  },
+  {
+    "revision": "847fcded08c51d3a9a2f",
+    "url": "/Portfolio/static/js/runtime-main.3312ea12.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ad7295b226a98ead97819c5e888ba8f9",
+    "url": "/Portfolio/static/media/BIT.ad7295b2.jfif"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "7b9044131c21c4f36c2805c8995c9e7a",
+    "url": "/Portfolio/static/media/Rakuten.7b904413.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "f60e4fd94702a51cdca91525e61c1b98",
+    "url": "/Portfolio/static/media/harvardLogo.f60e4fd9.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "a798cdf1f7b64859d81c08ef2b625a6c",
+    "url": "/Portfolio/static/media/oracle.a798cdf1.jfif"
+  },
+  {
+    "revision": "f5b9c664252b737d150f8cba76a5972c",
+    "url": "/Portfolio/static/media/paniit.f5b9c664.png"
+  },
+  {
+    "revision": "10db07c4c595e158c6d242a2af2d082c",
+    "url": "/Portfolio/static/media/printalytix.10db07c4.png"
+  },
+  {
+    "revision": "c2bc4ed3668414139b63369627dd63e8",
+    "url": "/Portfolio/static/media/siemens.c2bc4ed3.jfif"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
